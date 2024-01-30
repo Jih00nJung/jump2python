9c44883172245ac46c07c5cd8ddabe738c2c1afe
burningcake <wjdwlgns11q@gmail.com>
2024-01-30 21:53:48 +0900

    2-5

--- 02-2/multistring.py
+++ 02-2/multistring.py
@@ -1,5 +1,15 @@
 # multistring.py
 
 print("=" * 50)
-print("My Program")
+print("my program")
 print("=" * 50)
+
+# 문자열 삽입
+print(",".join('abcd'))
+print("=" * 50)
+
+# 문자열 추가
+a = [1, 2, 3]
+a.append(4)
+print(a)
+
